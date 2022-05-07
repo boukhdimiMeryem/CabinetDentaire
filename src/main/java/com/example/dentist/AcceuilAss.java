530a66de328102d500279fc189de95b3993898cb
User <elmajoudi.yasmine@student.emi.ma>
2022-05-07 02:01:45 +0200

    update

--- src/main/java/com/example/dentist/AcceuilAss.java
+++ src/main/java/com/example/dentist/AcceuilAss.java
@@ -1,13 +1,34 @@
 package com.example.dentist;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AcceuilAss implements Initializable {
+    @FXML
+    private AnchorPane  Friday;
+
+    @FXML
+    private AnchorPane Monday;
+
+    @FXML
+    private AnchorPane Saturday;
+
+    @FXML
+    private AnchorPane Thursday;
+
+    @FXML
+    private AnchorPane Tuesday;
+
+    @FXML
+    private AnchorPane Wednesday;
+
     @FXML
     private Button btn_logout;
 
@@ -17,28 +38,14 @@ public class AcceuilAss implements Initializable {
     @FXML
     private Button zoom;
 
-    public int nbDays(String months)
-    {
-        int i=0;
-        switch(months){
-            case "january": i=31;
-            case "february": i=28;
-            case "march": i=31;
-            case "april": i=30;
-            case "may": i=31;
-            case "june": i=30;
-            case "july": i=31;
-            case "august": i=30;
-            case "september": i=31;
-            case "october": i=30;
-            case "november": i=31;
-            case "december": i=30;
-        }
-        return i;
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println(nbDays("january"));
     }
+
+    public void test(ActionEvent actionEvent) {
+        Friday.setStyle("-fx-background-color:'blue'");
+
+    }
+
 }
